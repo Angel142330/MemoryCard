7d6a7d51abc78d241e90573cfc4d852f8e24fea7
angel142330@gmail.com <angel142330@gmail.es>
2023-11-11 22:13:23 +0100

    cambios menores

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
-let cartasDestapadas = 0;
-let temporizador = false;
-let pares = 0;
-let timerInicial =60;
-let timer = 60;
-let puntaje = 0;
-let movimientos = 0;
-let mostrarTiempo = document.getElementById("restante");
-let mostrarPuntaje = document.getElementById("puntaje");
-let mostrarMovimientos = document.getElementById("movimientos");
-let contenedor = document.getElementById("contenedor");
-let reiniciarBtn = document.getElementById("reiniciar-btn");
-reiniciarBtn.addEventListener("click", reiniciarJuego);
-let nivelActual = 'facil'; 
-let tiempoRegresivo;
+let cantidadCartasDestapadas  = 0;
+let temporizadorActivo  = false;
+let paresEncontrados  = 0;
+let tiempoInicial  = 60;
+let tiempoRestante  = 60;
+let puntajeActual  = 0;
+let movimientosRealizados  = 0;
+let numeros = [];
 
-// Baraja y asigna números a las cartas
-let numeros = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
-numeros = numeros.sort(function () {
-  return Math.random() - 0.3;
-});
+let mostrarTiempoRestante  = document.getElementById("restante");
+let mostrarPuntajeActual  = document.getElementById("puntaje");
+let mostrarMovimientosRealizados  = document.getElementById("movimientos");
+let contenedorCartas  = document.getElementById("contenedor");
+let botonReiniciar  = document.getElementById("reiniciar-btn");
+let nivelActual = 'facil';
+let intervaloTiempoRegresivo;
+
 
 // Agrega eventos de clic a los botones
-let botones = document.querySelectorAll(".custom-btn");
-botones.forEach((button, i) => {
-  button.addEventListener("click", function () {
-    girar(i);
+let botonesCartas  = document.querySelectorAll(".custom-btn");
+botonesCartas .forEach((boton, i) => {
+  boton.addEventListener("click", function () {
+    girarCarta(i);
   });
 });
 
+botonReiniciar .addEventListener("click", reiniciarJuego);
+
 // Función para contar el tiempo regresivo
 function contarTiempo() {
-  tiempoRegresivo = setInterval(() => {
-    mostrarTiempo.innerHTML = `Tiempo restante: ${timer} segundos`;
-    timer--;
-    if (timer < 0) {
+  intervaloTiempoRegresivo = setInterval(() => {
+    mostrarTiempoRestante .innerHTML = `Tiempo restante: ${tiempoRestante } segundos`;
+    tiempoRestante --;
+    if (tiempoRestante  < 0) {
       // Detener el temporizador cuando se agota el tiempo
-      clearInterval(tiempoRegresivo);
+      clearInterval(intervaloTiempoRegresivo);
       bloquearTarjetas(numeros);
-      mostrarTiempo.innerHTML = "¡Se agotó el tiempo!";
-      reiniciarBtn.classList.remove("d-none"); 
+      mostrarTiempoRestante .innerHTML = "¡Se agotó el tiempo!";
+      botonReiniciar .classList.remove("d-none");
     }
   }, 1000);
 }
@@ -52,13 +50,12 @@ function bloquearTarjetas(numeros) {
   });
 }
 
-function girar(id) {
-  if (temporizador == false) {
+function girarCarta(id) {
+  if (temporizadorActivo  == false) {
     contarTiempo();
-    temporizador = true;
+    temporizadorActivo  = true;
   }
-  
- 
+
   const card = document.getElementById(id);
 
   if (!card.classList.contains("flipped")) {
@@ -66,33 +63,33 @@ function girar(id) {
     card.classList.add("flipped");
 
     setTimeout(() => {
-     
-      if (cartasDestapadas == 0) {
+
+      if (cantidadCartasDestapadas  == 0) {
         primeraEleccion = numeros[id];
         card.innerHTML = `<img src="./image/${primeraEleccion}.png" alt="" class="carta-img">`;
 
         // Deshabilitar botón
         card.disabled = true;
-        cartasDestapadas++;
+        cantidadCartasDestapadas ++;
 
         primerId = id; // Almacena temporalmente la identificación de la primera carta destapada
-      } else if (cartasDestapadas == 1) {
+      } else if (cantidadCartasDestapadas  == 1) {
         segundaEleccion = numeros[id];
         card.innerHTML = `<img src="./image/${segundaEleccion}.png" alt="" class="carta-img">`;
         // Deshabilitar botón
         card.disabled = true;
-        cartasDestapadas++;
+        cantidadCartasDestapadas ++;
 
         segundoId = id; // Almacena temporalmente la identificación de la segunda carta destapada
 
-        movimientos++;
-        mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
+        movimientosRealizados ++;
+        mostrarMovimientosRealizados .innerHTML = `Movimientos: ${movimientosRealizados }`;
 
         if (primeraEleccion == segundaEleccion) {
-          cartasDestapadas = 0;
-          pares++;
-          puntaje++;
-          mostrarPuntaje.innerHTML = `Puntaje: ${puntaje}`;
+          cantidadCartasDestapadas  = 0;
+          paresEncontrados ++;
+          puntajeActual ++;
+          mostrarPuntajeActual .innerHTML = `Puntaje: ${puntajeActual }`;
         } else {
           setTimeout(() => {
             // Recupera las identificaciones almacenadas para revertir la animación
@@ -104,76 +101,70 @@ function girar(id) {
             card2.innerHTML = `<span class="interrogation">?</span>`;
             card1.disabled = false;
             card2.disabled = false;
-            cartasDestapadas = 0;
+            cantidadCartasDestapadas  = 0;
           }, 500);
         }
       }
 
-      if (pares == niveles[nivelActual].pares) {
-        clearInterval(tiempoRegresivo);
-        mostrarTiempo.innerHTML = `¡Wooo! 🚀 Lograste completar el desafío en tan solo ${
-          timerInicial - timer - 1
+      if (paresEncontrados  == niveles[nivelActual].pares) {
+        clearInterval(intervaloTiempoRegresivo);
+        mostrarTiempoRestante .innerHTML = `¡Wooo! 🚀 Lograste completar el desafío en tan solo ${
+          tiempoInicial  - tiempoRestante  - 1
         } segundos.`;
-        mostrarPuntaje.innerHTML = `¡Increíble! 🌟 Tu puntaje es ${puntaje} 🎉.`;
-        mostrarMovimientos.innerHTML = `¡Muy astuto! 🤓 Realizaste ${movimientos} movimientos estratégicos.`;
-       
+        mostrarPuntajeActual .innerHTML = `¡Increíble! 🌟 Tu puntaje es ${puntajeActual } 🎉.`;
+        mostrarMovimientosRealizados .innerHTML = `¡Muy astuto! 🤓 Realizaste ${movimientosRealizados } movimientos estratégicos.`;
+        botonReiniciar .classList.remove("d-none");
       }
-    }, 100); 
+    }, 100);
   }
 }
 
-
-
 function reiniciarJuego() {
-  clearInterval(tiempoRegresivo);
+  clearInterval(intervaloTiempoRegresivo);
   //Reinicia variables
-  cartasDestapadas = 0;
-  temporizador = false;
-  pares = 0;
-  timer = timerInicial;
-  puntaje = 0;
-  movimientos = 0;
-  mostrarTiempo.innerHTML = `Tiempo restante: ${timer} segundos`;
-  mostrarPuntaje.innerHTML = `Puntaje: ${puntaje}`;
-  mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
+  cantidadCartasDestapadas  = 0;
+  temporizadorActivo  = false;
+  paresEncontrados  = 0;
+  tiempoRestante  = tiempoInicial ;
+  puntajeActual  = 0;
+  movimientosRealizados  = 0;
+  mostrarTiempoRestante .innerHTML = `Tiempo restante: ${tiempoRestante } segundos`;
+  mostrarPuntajeActual .innerHTML = `Puntaje: ${puntajeActual }`;
+  mostrarMovimientosRealizados .innerHTML = `Movimientos: ${movimientosRealizados }`;
 
   // Baraja nuevamente los números
   numeros = numeros.sort(() => Math.random() - 0.5);
 
   // Habilita los botones y restaura su contenido
-  botones.forEach((button, i) => {
+  botonesCartas .forEach((button, i) => {
     button.classList.remove("flipped");
     button.innerHTML = `<span class="interrogation">?</span>`;
     button.disabled = false;
   });
-
-  
+  botonReiniciar .classList.add("d-none");
 }
 
-
 let niveles = {
-  facil: { pares: 8, tiempo: 10 },
+  facil: { pares: 8, tiempo: 60 },
   medio: { pares: 10, tiempo: 75 },
   dificil: { pares: 12, tiempo: 90 }
 };
 
-
 let nivelBtns = document.getElementById("nivel-btns");
 nivelBtns.addEventListener("click", function (event) {
-    if (event.target.classList.contains("nivel-btn")) {
-        let nivel = event.target.id;
-        iniciarJuego(nivel);
-    }
+  if (event.target.classList.contains("nivel-btn")) {
+    let nivel = event.target.id;
+    iniciarJuego(nivel);
+  }
 });
-
 
 function iniciarJuego(nivel) {
   reiniciarJuego();
   nivelActual = nivel;
-  puntaje = 0;
-  pares = 0;
-  movimientos = 0;
-  temporizador = false;
+  puntajeActual  = 0;
+  paresEncontrados  = 0;
+  movimientosRealizados  = 0;
+  temporizadorActivo  = false;
   let configuracion = niveles[nivel];
 
   // Genera los números para el nivel seleccionado
@@ -183,25 +174,22 @@ function iniciarJuego(nivel) {
   }
   numeros = numeros.sort(() => Math.random() - 0.5);
 
-  
-  timerInicial = configuracion.tiempo;
-  timer = timerInicial;
-
+  tiempoInicial  = configuracion.tiempo;
+  tiempoRestante  = tiempoInicial ;
 
   let contenedor = document.getElementById("contenedor");
-  contenedor.innerHTML = ''; 
+  contenedor.innerHTML = '';
   contenedor.className = 'grid ' + nivel;
   for (let i = 0; i < numeros.length; i++) {
- 
+
     let boton = document.createElement("button");
     boton.id = i;
     boton.className = "custom-btn";
     boton.innerHTML = `<span class="interrogation">?</span>`;
     boton.addEventListener("click", function () {
-      girar(i);
+      girarCarta(i);
     });
     contenedor.appendChild(boton);
   }
-  botones = document.querySelectorAll(".custom-btn"); // Actualizamos la lista de botones
-  
+  botonesCartas  = document.querySelectorAll(".custom-btn"); // Actualizamos la lista de botones
 }
